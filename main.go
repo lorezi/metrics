a4db95e4e5cbb4807221cb29db157805ec3b7fdc
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2022-07-06 18:08:51 +0100

    adds endpoints for instrumentation

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // computeSum generates a SHA256 hash of the body
@@ -14,5 +21,37 @@ func computeSum(body []byte) []byte {
 }
 
 func main() {
+	port := 8082
+	readTimeout := time.Millisecond * 500
+	writeTimeout := time.Millisecond * 500
 
+	mux := http.NewServeMux()
+
+	httpMetrics := NewHttpMetrics()
+	instrumentedHash := InstrumentHandler(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if r.Body != nil {
+			body, _ := ioutil.ReadAll(r.Body)
+			fmt.Fprintf(w, "%s", computeSum(body))
+		}
+	}, httpMetrics)
+
+	mux.HandleFunc("/hash", instrumentedHash)
+
+	mux.Handle("/metrics", promhttp.Handler())
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20,
+		Handler:        mux,
+	}
+
+	log.Printf("Listening on: %d", port)
+	s.ListenAndServe()
 }
